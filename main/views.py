ad48042ab9ebe57b2f76d82fb7bc640b79e7e96a
kadosh <Noone19780506@outlook.com>
2024-09-02 21:04:25 +0300

    02.09.2024

--- main/views.py
+++ main/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About us')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Самый супер классная магазин'
+    }
+    return render(request, 'main/about.html', context)
