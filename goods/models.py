b8fb39d516a64fdcf0b7274a7cc3960c569383d2
kadosh <Noone19780506@outlook.com>
2024-09-04 22:29:46 +0300

    04.09.2024

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self) -> str:
+        return f'{self.name} Quantity -{self.quantity}'
