f4967a2fd7bc3564484452bc00b1e1c7522a69c6
kadosh <Noone19780506@outlook.com>
2024-09-12 22:28:07 +0300

    12.09.2024

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
-from app.settings import DEBUG
+from app import settings
+from django.conf.urls.static import static
 
 from main import views
 
@@ -11,7 +12,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns +=[
         path("__debug__/", include(debug_toolbar_urls())),
     ]
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
